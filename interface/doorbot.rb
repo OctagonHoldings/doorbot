54f828930dcee30c7fd127737340a4c1d8ac25a8
Nicole Aptekar <me@nicolation.net>
2014-10-20 23:06:24 -0700

    added uikit, some silly pages

--- interface/doorbot.rb
+++ interface/doorbot.rb
@@ -1,5 +1,28 @@
 require 'sinatra'
+require 'haml'
 
-get '/hi' do
-  "Hello World!"
+helpers do
+  def protected!
+    return if authorized?
+    headers['WWW-Authenticate'] = 'Basic realm="Restricted Area"'
+    halt 401, "Not authorized\n"
+  end
+
+  def authorized?
+    @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+    @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == ['admin', '0rbit']
+  end
+end
+
+get '/' do
+  haml :index, :format => :html5
+end
+
+get '/potatoes' do
+  haml :potatoes, :format => :html5
+end
+
+get '/admin' do
+  protected!
+  haml :admin, :format => :html5
 end
