d869deddae3d891ae9943ef6dff9704242f53ba8
Ian Baker <ian@sonic.net>
2014-10-26 13:55:43 -0700

    Added specs for existing endpoints, test auth

--- interface/spec/app_spec.rb
+++ interface/spec/app_spec.rb
@@ -1,8 +1,53 @@
 require File.expand_path '../spec_helper.rb', __FILE__
+require 'base64'
 
-describe "My Sinatra Application" do
-  it "should allow accessing the home page" do
+describe "Doorbot" do
+  it "has a homepage" do
     get '/'
     expect(last_response).to be_ok
   end
+
+  it "has a potatoes" do
+    get '/potatoes'
+    expect(last_response).to be_ok
+  end
+
+  context 'when not logged in' do
+    it 'does not allow access to /admin without a password' do
+      get '/admin'
+      expect(last_response.status).to eq 401
+      expect(last_response.header['WWW-Authenticate']).to eq 'Basic realm="Restricted Area"'
+    end
+
+    it 'does not allow access to /admin/list without a password' do
+      get '/admin'
+      expect(last_response.status).to eq 401
+      expect(last_response.header['WWW-Authenticate']).to eq 'Basic realm="Restricted Area"'
+    end
+  end
+
+  context 'when logged in as an admin' do
+    let(:rack_env) do
+      {
+        'HTTP_AUTHORIZATION' => "Basic #{Base64.encode64('admin:password').chomp}"
+      }
+    end
+
+    before do
+      ENV['DOORBOT_ADMIN_USER'] = 'admin'
+      ENV['DOORBOT_ADMIN_PASSWORD'] = 'password'
+    end
+
+    it 'allows admin access' do
+      get '/admin', {}, rack_env
+      expect(last_response).to be_ok
+    end
+
+    it 'allows access to /admin/list' do
+      get '/admin', {}, rack_env
+      expect(last_response).to be_ok
+    end
+
+  end
+
 end
