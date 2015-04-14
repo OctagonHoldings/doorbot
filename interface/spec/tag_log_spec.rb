5455dfcdeb54a6278e02ba23f1dabbe764750bc6
Ian Baker <ian@sonic.net>
2015-04-13 23:16:38 -0700

    Adding tag_log_spec because apparently I forgot to commit it.

---
+++ interface/spec/tag_log_spec.rb
@@ -0,0 +1,34 @@
+require File.expand_path '../spec_helper.rb', __FILE__
+require 'base64'
+
+describe "Log" do
+  context 'when not logged in' do
+
+    it 'does not allow access to /admin/log without a password' do
+      get '/admin/log'
+      expect(last_response.status).to eq 401
+      expect(last_response.header['WWW-Authenticate']).to eq 'Basic realm="Restricted Area"'
+    end
+  end
+
+  context 'when logged in as an admin' do
+    let(:rack_env) do
+      {
+        'HTTP_AUTHORIZATION' => "Basic #{Base64.encode64('admin:password').chomp}"
+      }
+    end
+
+    before do
+      ENV['DOORBOT_ADMIN_USER'] = 'admin'
+      ENV['DOORBOT_ADMIN_PASSWORD'] = 'password'
+    end
+
+    it 'shows the log' do
+      get '/admin/log', {}, rack_env
+      expect(last_response).to be_ok
+    end
+
+
+
+  end
+end
