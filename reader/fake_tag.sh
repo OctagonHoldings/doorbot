961afc5350efd54b7843a257675bcfcb31e73a85
Ian Baker <ian@sonic.net>
2015-04-20 23:17:27 -0700

    Updated tag tester to use tags in /tmp

--- reader/fake_tag.sh
+++ reader/fake_tag.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-TAGFILE=`dirname $0`/test.tags
+TAGFILE=/tmp/guardian-test-numbers
 for((i=1;i<=`wc -l $TAGFILE | sed s/'[ ][ ][ ]*'//g | cut -f 1 -d ' '`;i++));
 do
     head -n $i $TAGFILE | tail -n 1
-    sleep 1
 done
 cat -
