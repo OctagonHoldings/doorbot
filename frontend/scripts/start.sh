b74c897139b7c21353075dee2016825c4ba7e63e
Ian Baker <ian@sonic.net>
2017-02-25 21:45:32 -0800

    Added a start script for running on boot

---
+++ frontend/scripts/start.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+cd /home/pi/doorbot/frontend
+
+export PATH=$PATH:/home/pi/.rbenv/shims
+
+bundle exec doorbot.rb -o 0.0.0.0 -p 8080 2>&1 | logger -t doorbot-web &
+bundle exec guardian.rb 2>&1 | logger -t doorbot-guardian &
