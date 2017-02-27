bf4d2947012cf423c9917330b9639ca57e2b26ff
Ian Baker <ian@sonic.net>
2017-02-27 00:59:36 -0800

    Added /usr/local/bin to path so guardian can find the gpio command

--- frontend/scripts/start.sh
+++ frontend/scripts/start.sh
@@ -2,7 +2,7 @@
 
 cd /home/pi/doorbot/frontend
 
-export PATH=$PATH:/home/pi/.rbenv/shims
+export PATH=$PATH:/home/pi/.rbenv/shims:/usr/local/bin
 
 bundle exec doorbot.rb -o 0.0.0.0 -p 8080 2>&1 | logger -t doorbot-web &
 bundle exec guardian.rb 2>&1 | logger -t doorbot-guardian &
