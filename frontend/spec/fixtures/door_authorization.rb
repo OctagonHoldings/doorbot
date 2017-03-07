f68e5f0872be97dc751ab23b70ddd97d4a6f1003
Nicole Aptekar <me@nicolation.net>
2017-03-06 21:48:43 -0800

    updated ruby to 2.3.3 and dealt with the consequences

--- frontend/spec/fixtures/door_authorization.rb
+++ frontend/spec/fixtures/door_authorization.rb
@@ -2,7 +2,7 @@ require 'dm-sweatshop'
 require 'ffaker'
 
 DoorAuthorization.fixture(:active) {{
-  name:        Faker::Name.name,
+  name:        FFaker::Name.name,
   card_type:   %w(clipper mifare)[rand(2)],
   card_number: /\d{10}/.gen,
   expires_at:  nil,
@@ -12,7 +12,7 @@ DoorAuthorization.fixture(:active) {{
 }}
 
 DoorAuthorization.fixture(:inactive) {{
-  name:        Faker::Name.name,
+  name:        FFaker::Name.name,
   card_type:   %w(clipper mifare)[rand(2)],
   card_number: /\d{10}/.gen,
   expires_at:  nil,
@@ -22,7 +22,7 @@ DoorAuthorization.fixture(:inactive) {{
 }}
 
 DoorAuthorization.fixture(:expired) {{
-  name:        Faker::Name.name,
+  name:        FFaker::Name.name,
   card_type:   %w(clipper mifare)[rand(2)],
   card_number: /\d{10}/.gen,
   expires_at:  Time.now - 10*86400,   # 10 days ago
--- frontend/spec/fixtures/tag_log.rb
+++ frontend/spec/fixtures/tag_log.rb
@@ -4,7 +4,7 @@ require 'ffaker'
 TagLog.fixture(:opened) {{
   card_type:   %w(clipper rfid)[rand(2)],
   card_number: /\d{10}/.gen,
-  name:        Faker::Name.name,
+  name:        FFaker::Name.name,
   created_at:  Time.now,
   door_opened: true,
 }}
@@ -12,7 +12,7 @@ TagLog.fixture(:opened) {{
 TagLog.fixture(:not_opened) {{
   card_type:   %w(clipper rfid)[rand(2)],
   card_number: /\d{10}/.gen,
-  name:        Faker::Name.name,
+  name:        FFaker::Name.name,
   created_at:  Time.now,
   door_opened: false,
 }}
