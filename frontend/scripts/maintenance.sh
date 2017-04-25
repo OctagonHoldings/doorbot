a3a1f16532f13d3f92acb8bb7c149e565ed97d6d
doorbot <doorbot@capelis.dj>
2017-04-24 20:54:44 -0700

    Updated maintenance scripts

--- frontend/scripts/maintenance.sh
+++ frontend/scripts/maintenance.sh
@@ -4,4 +4,4 @@ cd /home/pi/doorbot/frontend
 
 export PATH=$PATH:/home/pi/.rbenv/shims:/usr/local/bin
 
-bundle exec maintenance 2>&1 | logger -t doorbot-maintenance &
+bundle exec maintenance.rb 2>&1 | logger -t doorbot-maintenance
